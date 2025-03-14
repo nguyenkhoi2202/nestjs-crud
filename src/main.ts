820e600bc8720056554720314477d6cbd302480f
nguyenkhoi2202 <63914061+nguyenkhoi2202@users.noreply.github.com>
2025-03-14 16:22:19 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,50 @@
 
-declare const module: any;
+// declare const module: any;
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import serverlessExpress from '@vendia/serverless-express';
+// import { Callback, Context, Handler } from 'aws-lambda';
+
+// let server: Handler;
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+
+//   await app.init();
+  
+//   const expressApp = app.getHttpAdapter().getInstance();
+//   server = serverlessExpress({ app: expressApp });
+
+//   if (module.hot) {
+//     module.hot.accept();
+//     module.hot.dispose(() => app.close());
+//   }
+// }
+
+// export const handler: Handler = (event: any, context: Context, callback: Callback) => {
+//   if (!server) {
+//     bootstrap().then(() => server(event, context, callback));
+//   } else {
+//     server(event, context, callback);
+//   }
+// };
+
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
 
 let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-
+  app.enableCors();
   await app.init();
-  
   const expressApp = app.getHttpAdapter().getInstance();
   server = serverlessExpress({ app: expressApp });
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 
 export const handler: Handler = (event: any, context: Context, callback: Callback) => {
@@ -29,5 +54,3 @@ export const handler: Handler = (event: any, context: Context, callback: Callbac
     server(event, context, callback);
   }
 };
-
-bootstrap();
